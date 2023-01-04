ca094a689376997c81e06550ad6ad84a5200d95d
EUS-VICTORY <cenessence72@gmail.com>
2023-01-03 19:43:39 -0800

    Python_loops_functions

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def add(a, b):
-    if i = a + b:
-       return i
+       return (a + b)
--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    if i = a ^ b:
-        return i
+    return (a ** b)
