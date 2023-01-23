5e50d65bff8a2f35f3911b7a575fbca91a9085ce
EUS-VICTORY <cenessence72@gmail.com>
2023-01-23 15:40:00 -0800

    Python-Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """print the element of x
+
+    Args:
+    my_list(list):the list  to print element from
+    x:the number of element of my_list to print
+    Returns: 
+    the number of elements printed
+    """
     count = 0
     for i in range(x):
         try:
@@ -8,3 +16,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             print()
     return count
+
