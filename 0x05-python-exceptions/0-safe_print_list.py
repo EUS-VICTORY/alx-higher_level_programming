dd32d38c83df0b5af2ea7edbb9c3fcec9e29ec7b
EUS-VICTORY <cenessence72@gmail.com>
2023-01-23 17:36:31 -0800

    Python-Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -15,7 +15,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             ret += 1
         except IndexError:
-            break
+            pass
     print("")
     return ret
 
