7fa632052eb474ad031076ebcedc738c5d545ecb
EUS-VICTORY <cenessence72@gmail.com>
2023-01-24 16:47:46 -0800

    python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines a class named Square"""
+
+
+class Square:
+    """defines a function called _init_"""
+    def _init_(self, size):
+    """initialises size of self with size"""
+    self._size = size
+
