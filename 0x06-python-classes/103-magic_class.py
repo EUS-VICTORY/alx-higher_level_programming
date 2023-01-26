cdece6a36473d82c17419fea86fec343992fc4b5
EUS-VICTORY <cenessence72@gmail.com>
2023-01-25 17:09:39 -0800

    Python-Classes

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,21 +1,37 @@
 #!/usr/bin/python3
-"""declares a class called magic class"""
+""" declares a class called MagicClass """
+
 import math
 
-class MagiClass:
-    """declaration of _init_ function"""
-    def _init_(self, radius=0):
-        """initialisation of _MagiClass_ radius with 0"""
-        self._MagicClass_radius = 0
-        if type(radius) is not int and type radius is not float:
-            raise TypeError("radius must be a number")
-        self._MagicClass_radius = radius
-    """defines a function named area"""
-    def area(self):
-        """returns area"""
-        return self._MagicClass_radius ** 2 * mathpie
-    """defines a function named circumference"""
-    def circumference(self):
-        """returns circumference"""
-        return 2 * mathpie * self._MagicClass_radius
+
+class MagicClass:
+    
+    """ declaration of __init__ function """
+    
+    def __init__(self, radius=0):
         
+        """ initialization of _MagicClass__radius with 0 """
+        
+        self._MagicClass__radius = 0
+        
+        if type(radius) is not int and type(radius) is not float:
+            
+            raise TypeError("radius must be a number")
+        
+        self._MagicClass__radius = radius
+        
+    """ defines a function named area """
+    
+    def area(self):
+        
+        """ returns area """
+        
+        return self._MagicClass__radius ** 2 * math.pi
+    
+    """ defines a function named circumference """
+    
+    def circumference(self):
+        
+        """ returns circumference """
+        
+        return 2 * math.pi * self._MagicClass__radius
