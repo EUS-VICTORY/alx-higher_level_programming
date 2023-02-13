300f07e2adaf058b63e8c7f4dc94252af784299c
EUS-VCITORY <cenessence72@gmail.com>
2023-02-13 18:33:04 +0100

    'Python-almost_a_circle'

---
+++ 0x0C-python-almost_a_circle/5-main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" 5-main """
+from models.rectangle import Rectangle
+
+if __name__ == "__main__":
+
+	r1 = Rectangle(4, 6, 2, 1, 12)
+	print(r1)
+
+	r2 = rectangle(5, 5, 1))
+	print(r2)
