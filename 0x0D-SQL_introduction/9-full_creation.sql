c24c06767053503bfb50eac32a75ace863b60f97
EUS-VCITORY <cenessence72@gmail.com>
2023-03-18 23:31:25 +0100

    'SQL_introduction'

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- creates and fills a table `second_table` with attributes id, name, and score.
+-- Creates and fills a table second_table with attributes id, name, and score.
 Create TABLE IF NOT EXIST `second_table` (`id` INT, `name` VACHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
